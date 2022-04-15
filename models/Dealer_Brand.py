6729e557fef4346fc5a58fe5723f69b2cf97f0a0
zhangjun <ibmzhangjun@139.com>
2022-04-15 22:39:31 +0800

    Init

---
+++ models/Dealer_Brand.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#  #
+#  Copyright (C) 2021 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2021
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: Neptune
+
+from typing import Optional
+from sqlalchemy import Column, VARCHAR
+from sqlmodel import Field, SQLModel
+
+class dealer_brand(SQLModel, table=True):
