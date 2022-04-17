c02034b78c3e240d502dc12f9f8d3ed41a8d8e38
zhangjun <ibmzhangjun@139.com>
2022-04-17 10:41:32 +0800

    Init

--- services/customersService.py
+++ services/customersService.py
@@ -10,7 +10,7 @@
 #  @Software: Neptune-NG
 import collections
 import simplejson as json
-from sqlalchemy import func, or_, literal_column, column
+from sqlalchemy import func, and_, or_, literal_column, column
 
 from sqlmodel import Session, select, col
 
@@ -134,26 +134,53 @@ class customersService(object):
 
     def query_customers(self):
         queryjsonstr = '{' \
-                       '"queryfields":"customers.*",' \
+                       '"queryfields":"customers.first_name,customers.last_name",' \
+                       '"where_and":"customers.first_name != \'Jun\'",' \
+                       '"where_or":"customers.household_income > 80001, customers.gender == \'Female\'",' \
                        '"order_by":"customers.phone_number.asc(), customers.household_income.asc()",' \
+                       '"group_by":"last_name",' \
                        '"limit":2,' \
-                       '"offset":2' \
+                       '"offset":2,' \
+                       '"withcount":"True"' \
                        '}'
         queryjson = json.loads(queryjsonstr)
         log.logger.debug('The query JSON is: %s' % queryjson)
+        #get querycolumns
         queryfields = queryjson['queryfields'].replace(' ','')
         if "*" in queryfields:
             queryfields="customers."+",customers.".join(tuple(customers.__fields__.keys()))
-        querycolumns = eval(queryfields)
+        statement = select(from_obj=customers, columns=eval(queryfields))
+        #get where
+        whereandfields = queryjson['where_and']
+        whereandfieldslist = tuple(filter(None,whereandfields.replace(' ','').split(',')))
+        print(whereandfieldslist)
+        for whereand in whereandfieldslist:
+            statement = statement.where(eval(whereand))
+        whereorfields = queryjson['where_or']
+        whereorfieldslist = tuple(filter(None, whereorfields.replace(' ', '').split(',')))
+        print(whereorfieldslist)
+        whereorfieldscolumns = []
+        for whereor in whereorfieldslist:
+            whereorfieldscolumns.append(eval(whereor))
+        statement = statement.where(or_(whereorfieldscolumns))
+        #get ordercolumns
         orderfields = queryjson['order_by']
         orderfieldslist = tuple(filter(None,orderfields.replace(' ','').split(',')))
-        print(orderfieldslist)
-        ordercolumns = eval(orderfields)
+        for order in orderfieldslist:
+            statement = statement.order_by(eval(order))
+        #get group_by
+        #get limit & offset
+        statement = statement.limit(queryjson['limit']).offset(queryjson['offset'])
+
+
+        log.logger.debug(statement)
 
         try:
             engine = dbengine.DBEngine().connect()
             with Session(engine) as session:
-                statement = select(from_obj=customers, columns=querycolumns).where(customers.first_name != 'Jun', customers.household_income > 80001).where(or_(customers.last_name != 'Zhang')).order_by(ordercolumns).limit(queryjson['limit']).offset(queryjson['offset'])
+                statement = select(customers).where(customers.first_name != 'Jun', customers.household_income > 80001).where(or_(customers.last_name != 'Zhang',customers.gender == 'Female')).order_by(eval("customers.phone_number.asc()"),eval("customers.household_income.asc()")).limit(queryjson['limit']).offset(queryjson['offset'])
+                log.logger.debug("1111111 : %s" % statement)
+                #statement = select(from_obj=customers, columns=querycolumns).where(customers.first_name != 'Jun',customers.household_income > 80001).where(or_(customers.last_name != 'Zhang')).order_by(ordercolumns).limit(queryjson['limit']).offset(queryjson['offset'])
                 result = session.exec(statement).all()
                 #log.logger.debug('query_customers() result is : %s' % result)
                 return result
