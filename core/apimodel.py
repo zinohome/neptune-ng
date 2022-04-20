87e7cb6f6fe354fb77a795aa2b8fa49757cbf664
zhangjun <ibmzhangjun@139.com>
2022-04-20 10:35:25 +0800

    fast api

--- core/apimodel.py
+++ core/apimodel.py
@@ -10,40 +10,37 @@
 #  @Software: Neptune
 
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Required
 from pydantic.class_validators import Optional
 from pydantic.types import Json, List, Dict
 
 from config import config
 
 class TableQueryBody(BaseModel):
-    queryfields: Optional[str] = None  # eg. "Customers.first_name,Customers.last_name,Customers.customer_id"
+    queryfields: Optional[str] = '*'  # eg. "Customers.first_name,Customers.last_name,Customers.customer_id"
     distinct: Optional[bool] = None
     where: Optional[str] = None
     order_by: Optional[str] = None
     group_by: Optional[str] = None
-    limit: Optional[int] = None
-    offset: Optional[int] = None
-    include_count: Optional[bool] = None
-    count_only: Optional[bool] = None
+    limit: Optional[int] = 10
+    offset: Optional[int] = 0
+    include_count: Optional[bool] = True
+    count_only: Optional[bool] = False
     class Config:
         title = 'Table Query Model'
         anystr_strip_whitespace = True
-        optional_by_default = False
 
 class TableBody(BaseModel):
     data: List[Dict] = None
     class Config:
         title = 'Table Model'
         anystr_strip_whitespace = True
-        optional_by_default = False
 
 class RecordBody(BaseModel):
     data: Dict = None
     class Config:
         title = 'Record Model'
         anystr_strip_whitespace = True
-        optional_by_default = False
 
 class PutRecordBody(BaseModel):
     data: Dict = None
@@ -51,5 +48,4 @@ class PutRecordBody(BaseModel):
     class Config:
         title = 'Put Record Model'
         anystr_strip_whitespace = True
-        optional_by_default = False
 
